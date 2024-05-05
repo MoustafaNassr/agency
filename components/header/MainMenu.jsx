fe3ccd0b53b4f424efba30ee643f4750f52c97bc
Moustapha Nassr <moustaphanassr@Moustaphas-MacBook-Pro.local>
2024-05-06 00:06:34 +0400

    release version for insurance web application

---
+++ components/header/MainMenu.jsx
@@ -0,0 +1,209 @@
+"use client";
+
+import Link from "next/link";
+import {
+  menuItems,
+  pagesItems,
+  portfolioItems,
+  blogItems,
+  contactItems,
+} from "@/data/menu";
+
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const MainMenu = () => {
+  const pathname = usePathname();
+
+  const isActive = (link) => {
+    return pathname.replace(/\/\d+$/, "") === link.replace(/\/\d+$/, "");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg order-lg-2">
+      <button
+        className="navbar-toggler d-block d-lg-none"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span />
+      </button>
+      {/* End mobile collapse menu */}
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="d-block d-lg-none">
+            <div className="logo">
+              <Link href="/" className="d-block">
+                <Image
+                  src="/images/logo/logo_01.png"
+                  alt="logo"
+                  width={95}
+                  height={30}
+                />
+              </Link>
+            </div>
+          </li>
+          {/* End li */}
+
+          <li >
+            <a
+              // className="nav-link dropdown-toggle active-menu"
+              href="/"
+          
+              className={
+                menuItems.some((menu) =>
+                  menu.items.some((elm) => isActive(elm.link))
+                )
+                  ? "nav-link  active-menu"
+                  : "nav-link "
+              }
+
+              // style={{color:'green'}}
+            >
+              Home
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <div className="row">
+                  {menuItems.map((menu, index) => (
+                    <div className="col-lg-4" key={index}>
+                      <div className="menu-column">
+                        <h6
+                          className={
+                            menu.items.some((elm) => isActive(elm.link))
+                              ? "mega-menu-title active-menu"
+                              : "mega-menu-title"
+                          }
+                        >
+                          {menu.title}
+                        </h6>
+                        <ul className="style-none mega-dropdown-list">
+                          {menu.items.map((item, index) => (
+                            <li key={index}>
+                              <Link
+                                href={item.link}
+                                className={`dropdown-item ${
+                                  isActive(item.link) ? "active" : ""
+                                }`}
+                              >
+                                <span>{item.title}</span>
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </li>
+            </ul>
+          </li>
+          {/* End li (home mega menu) */}
+
+          <li className="nav-item  dropdown">
+            <a
+              // className="nav-link dropdown-toggle"
+              className={
+                pagesItems.some((menu) =>
+                  menu.subItems.some((elm) => isActive(elm.link))
+                ) ||
+                isActive("/pages-menu/pricing") ||
+                isActive("/pages-menu/testimonials")
+                  ? "nav-link  active-menu"
+                  : "nav-link "
+              }
+              href="/pages-menu/about-us-v1"
+             
+            >
+about            </a>
+
+          
+          </li>
+          {/* End li (pages) */}
+
+          {/* services */}
+
+          <li className="nav-item  dropdown">
+            <a
+              // className="nav-link dropdown-toggle"
+              className={
+                pagesItems.some((menu) =>
+                  menu.subItems.some((elm) => isActive(elm.link))
+                ) ||
+                isActive("/pages-menu/pricing") ||
+                isActive("/pages-menu/testimonials")
+                  ? "nav-link  active-menu"
+                  : "nav-link "
+              }
+              href="/pages-menu/service-v1"
+             
+            >
+Services           </a>
+
+           
+          </li>
+          {/* End li (services) */} 
+
+    
+
+          <li className="nav-item dropdown">
+            <a
+              // className="nav-link dropdown-toggle"
+              className={
+                blogItems.some((elm) => isActive(elm.link))
+                  ? "nav-link dropdown-toggle active-menu"
+                  : "nav-link dropdown-toggle"
+              }
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              data-bs-auto-close="outside"
+              aria-expanded="false"
+            >
+              Blog
+            </a>
+            <ul className="dropdown-menu">
+              {blogItems.map((blog, index) => (
+                <li key={index}>
+                  <Link
+                    href={blog.link}
+                    className={`dropdown-item ${
+                      isActive(blog.link) ? "active" : ""
+                    }`}
+                  >
+                    <span>{blog.text}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+          {/* End li (blog) */}
+            {/* Contact start  */}
+         
+          {/* End li (contact) */}
+        </ul>
+        {/* End ul */}
+
+        {/* Mobile Content */}
+        <div className="mobile-content d-block d-lg-none">
+          <div className="d-flex flex-column align-items-center justify-content-center mt-70">
+            <Link
+              href="/contact/contact-v1"
+              className="btn-twentyOne fw-500 tran3s"
+            >
+              Contact us
+            </Link>
+          </div>
+        </div>
+        {/* /.mobile-content */}
+      </div>
+    </nav>
+  );
+};
+
+export default MainMenu;
